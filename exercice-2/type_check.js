9bcbd97d0f1bec557ab0d490e38116f0b226a255
jules <julesgabiam31@gmail.com>
2020-10-23 18:33:37 +0200

    exercice 2

---
+++ exercice-2/type_check.js
@@ -0,0 +1,28 @@
+
+function type_check_v1(arg1,arg2){
+    
+ console.log("typeof "+typeof(arg1));
+ if(typeof(arg1)== "object"){
+
+    if(Array.isArray(arg1)){
+        if(arg2=="array") return true;
+        return false;
+    }
+    else if(arg1==null){
+        if(arg2=="null") return true;
+        return false;
+    }
+
+    else if(arg2=="object") return true;
+    return false;
+
+
+
+ }else if(typeof(arg1)===arg2) {
+    return true;
+ }else{
+    return false;
+ }
+   
+   
+}
